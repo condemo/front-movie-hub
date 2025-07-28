a8ebce4814b1a619da3d72b7f3a37e57b12692a2
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-07-28 20:07:24 +0200

    inital PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -11,19 +11,73 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     VitePWA({
-      mode: "development",
-      base: "/",
-      srcDir: "src",
-      filename: "sw.ts",
-      includeAssets: ["/favicon.png"],
-      strategies: "injectManifest",
+      registerType: "autoUpdate",
+      includeAssets: ["/favicon.png", "/apple-touch-icon-180x180.png", "/maskable-icon-512x512.png"],
       manifest: {
         name: "Movie Hub",
         short_name: "Movie Hub",
         theme_color: "#ffffff",
         start_url: "/",
         display: "standalone",
+        orientation: "portrait",
+        lang: "es",
         background_color: "#ffffff",
+        icons: [
+          {
+            "src": "manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'style' || request.destination === 'script' ||
+             request.destination === 'worker',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-resources',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 24 * 60 * 60, // 60 days
+              },
+            },
+          },
+        ]
       }
     })
   ],
