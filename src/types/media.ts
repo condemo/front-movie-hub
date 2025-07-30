9c8e36a209da0a7a82a7e90ee0f034d65652ef78
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-07-30 22:19:03 +0200

    feat: added `Media` and `MediaType`

--- src/types/media.ts
+++ src/types/media.ts
@@ -1,10 +1,24 @@
 export type MediaResume = {
   Id: number
-  Type: string
+  Type: string | MediaType
   Title: string
   Genres: string
   Description: string
   Image: string
   Fav: boolean
   Viewed: boolean
+}
+
+export interface Media extends MediaResume {
+  Year: number
+  FirstAir: number
+  Seasons: number
+  Caps: number
+  Rating: number
+  Runtime: number
+}
+
+export enum MediaType {
+  Movie = "movie",
+  Series = "series"
 }
