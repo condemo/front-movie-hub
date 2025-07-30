f5232a1d717c6e5fac14a225e09c66700f89e675
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-07-30 16:22:56 +0200

    updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-f84911c5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ldnocd4ul2g"
+    "revision": "0.aauoctll36"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
