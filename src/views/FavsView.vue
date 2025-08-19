32f6a9837f4cc04bae04dcc34a6bef61b148b7e6
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-08-19 19:40:55 +0200

    feat:  booleans can be updated inside  and

--- src/views/FavsView.vue
+++ src/views/FavsView.vue
@@ -5,12 +5,29 @@ import MediaCard from '@/components/MediaCard.vue'
 
 const mediaStore = useMediaStore()
 const { favMedia } = storeToRefs(mediaStore)
+
+const swapMediaViewed = (index: number) => {
+  const md = favMedia.value[index]
+  mediaStore.updateMediaBooleans(md.id, { fav: md.fav, viewed: !md.viewed })
+}
+
+const swapMediaFav = (index: number) => {
+  const md = favMedia.value[index]
+  mediaStore.updateMediaBooleans(md.id, { fav: !md.fav, viewed: md.viewed })
+}
 </script>
 
 <template>
   <div class="flex flex-col space-y-1">
     <div v-if="favMedia.length > 0">
-      <MediaCard v-for="(media, i) in favMedia" :key="media.id" :media="media" :index="i" />
+      <MediaCard
+        v-for="(media, i) in favMedia"
+        :key="media.id"
+        :media="media"
+        :index="i"
+        @swap-media-viewed="swapMediaViewed"
+        @swap-media-fav="swapMediaFav"
+      />
     </div>
     <div v-else class="text-2xl font-bold text-primary">No hay favoritas todavía!!</div>
   </div>
--- src/views/ViewedView.vue
+++ src/views/ViewedView.vue
@@ -5,11 +5,28 @@ import MediaCard from '@/components/MediaCard.vue'
 
 const mediaStore = useMediaStore()
 const { viewedMedia } = storeToRefs(mediaStore)
+
+const swapMediaViewed = (index: number) => {
+  const md = viewedMedia.value[index]
+  mediaStore.updateMediaBooleans(md.id, { fav: md.fav, viewed: !md.viewed })
+}
+
+const swapMediaFav = (index: number) => {
+  const md = viewedMedia.value[index]
+  mediaStore.updateMediaBooleans(md.id, { fav: !md.fav, viewed: md.viewed })
+}
 </script>
 <template>
   <div class="flex flex-col space-y-1">
     <div v-if="viewedMedia.length > 0">
-      <MediaCard v-for="(media, i) in viewedMedia" :key="media.id" :media="media" :index="i" />
+      <MediaCard
+        v-for="(media, i) in viewedMedia"
+        :key="media.id"
+        :media="media"
+        :index="i"
+        @swap-media-viewed="swapMediaViewed"
+        @swap-media-fav="swapMediaFav"
+      />
     </div>
     <div v-else class="text-2xl font-bold text-primary">No hay vistas todavía!!</div>
   </div>
