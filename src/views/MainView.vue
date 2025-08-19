983a566b719b68646a6a8143e15ce4f37a09caf3
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-08-19 16:46:21 +0200

    chore: syntax tweaks

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -7,29 +7,29 @@ import { onMounted, onUnmounted, useTemplateRef } from 'vue'
 
 const mediaStore = useMediaStore()
 const { mediaList, loading, fetching } = storeToRefs(mediaStore)
-const scrollComponent = useTemplateRef("scrollComponent")
+const scrollComponent = useTemplateRef('scrollComponent')
 
 const swapMediaViewed = (index: number) => {
   mediaList.value[index].viewed = !mediaList.value[index].viewed
-  var md = mediaList.value[index]
+  const md = mediaList.value[index]
   mediaStore.updateMediaBooleans(md.id, { fav: md.fav, viewed: md.viewed })
 }
 
 const swapMediaFav = (index: number) => {
   mediaList.value[index].fav = !mediaList.value[index].fav
-  var md = mediaList.value[index]
+  const md = mediaList.value[index]
   mediaStore.updateMediaBooleans(md.id, { fav: md.fav, viewed: md.viewed })
 }
 
 onMounted(() => {
-  window.addEventListener("scroll", handleScroll)
+  window.addEventListener('scroll', handleScroll)
 })
 
 onUnmounted(() => {
-  window.removeEventListener("scroll", handleScroll)
+  window.removeEventListener('scroll', handleScroll)
 })
 
-const handleScroll = async (_: Event) => {
+const handleScroll = async () => {
   const element = scrollComponent.value as Element | null
   if (element) {
     if (element.getBoundingClientRect().bottom / 2 < window.innerHeight) {
@@ -45,8 +45,14 @@ const handleScroll = async (_: Event) => {
   <div>
     <LoadingSpinner v-if="loading" />
     <div ref="scrollComponent" v-else class="flex flex-col space-y-1">
-      <MediaCard v-for="(media, i) in mediaList" :key="media.id" :media="media" :index="i"
-        @swap-media-viewed="swapMediaViewed" @swap-media-fav="swapMediaFav" />
+      <MediaCard
+        v-for="(media, i) in mediaList"
+        :key="media.id"
+        :media="media"
+        :index="i"
+        @swap-media-viewed="swapMediaViewed"
+        @swap-media-fav="swapMediaFav"
+      />
     </div>
   </div>
 </template>
