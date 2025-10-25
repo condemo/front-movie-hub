418500beff43f9438903235d16e403d20fde48f8
Gustavo de los Santos <gustleo.dev@gmail.com>
2025-10-25 17:25:25 +0200

    change: `MediaCard` is full clickable

--- src/components/MediaCard.vue
+++ src/components/MediaCard.vue
@@ -1,27 +1,25 @@
 <template>
-  <div>
+  <div @click="toMediaResume">
     <div class="card card-side bg-base-100 h-40 max-w-full border-b-2 border-t-2 border-neutral">
       <figure class="w-1/3">
         <img :src="media.thumbnail" alt="Movie" />
       </figure>
-      <div class="card-body flex space-y-0">
-        <router-link :to="'/resume/' + media.id">
-          <div class="flex flex-row justify-between">
-            <h2 class="card-title truncate max-h-4 max-w-44 mx-auto">
-              {{ media.title }}
-            </h2>
-            <span class="font-bold text-xl text-error">{{
-              (Number(media.rating) / 10).toFixed(1)
-            }}</span>
-          </div>
-          <p class="font-bold text-base" :class="media.type === MediaType.Movie ? 'text-accent' : 'text-info'">
-            {{ media.type === MediaType.Movie ? 'Película' : 'Serie' }}
-          </p>
-          <p class="font-bold text-secondary">{{ media.genres }}</p>
-        </router-link>
+      <div class="card-body flex space-y-0 py-2">
+        <div class="flex flex-row justify-between">
+          <h2 class="card-title truncate max-h-4 max-w-44 mx-auto">
+            {{ media.title }}
+          </h2>
+          <span class="font-bold text-xl text-error">{{
+            (Number(media.rating) / 10).toFixed(1)
+          }}</span>
+        </div>
+        <p class="font-bold text-base" :class="media.type === MediaType.Movie ? 'text-accent' : 'text-info'">
+          {{ media.type === MediaType.Movie ? 'Película' : 'Serie' }}
+        </p>
+        <p class="font-bold text-secondary">{{ media.genres }}</p>
         <div class="card-actions flex flex-row justify-evenly mx-auto w-full space-x-3 *:w-10">
-          <FavButton :fav="media.fav || false" @click="$emit('swap-media-fav', index)" />
-          <ViewedButton :viewed="media.viewed || false" @click="$emit('swap-media-viewed', index)" />
+          <FavButton :fav="media.fav || false" @click.stop="$emit('swap-media-fav', index)" />
+          <ViewedButton :viewed="media.viewed || false" @click.stop="$emit('swap-media-viewed', index)" />
           <RecomendedIcon v-show="media.recomended" />
         </div>
       </div>
@@ -35,9 +33,14 @@ import type { PropType } from 'vue'
 import FavButton from './elements/FavButton.vue'
 import ViewedButton from './elements/ViewedButton.vue'
 import RecomendedIcon from './elements/RecomendedIcon.vue'
+import router from '@/router'
 
-defineProps({
+const props = defineProps({
   media: { type: Object as PropType<MediaResume>, required: true },
   index: { type: Number, required: true },
 })
+
+const toMediaResume = () => {
+  router.push("/resume/" + props.media.id)
+}
 </script>
